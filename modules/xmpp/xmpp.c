dda4256e939b19573db1db5956152f81739883d4
levin <levin108@gmail.com>
2011-07-14 22:10:22 +0800

    tested starting a jabber stream

--- modules/xmpp/xmpp.c
+++ modules/xmpp/xmpp.c
@@ -1,5 +1,6 @@
 #include <glib.h>
 #include "util.h"
+#include "eventloop.h"
 #include "account.h"
 #include "module.h"
 #include "info.h"
@@ -10,10 +11,63 @@
 #include "gtkutils.h"
 #include "tooltip.h"
 
+const gchar *jabber_server = "talk.l.google.com";
+
+static gboolean
+init_stream_cb(gint sk, gpointer user_data)
+{
+	gchar buf[BUF_LENGTH];
+	gint n;
+
+	if ((n = recv(sk, buf, strlen(buf), 0)) == -1) {
+		
+		hybrid_debug_error("xmpp", "init stream error.");
+
+		return FALSE;
+	}
+
+	buf[n] = '\0';
+
+	g_print("%s", buf);
+
+	return TRUE;
+}
+
+static gboolean
+init_connect_cb(gint sk, gpointer user_data)
+{
+	const gchar *msg;
+
+	/* send version. */
+	msg = "<?xml version='1.0' ?>";
+
+	if (send(sk, msg, strlen(msg), 0) == -1) {
+
+		hybrid_debug_error("xmpp", "send initial jabber request failed");
+
+		return FALSE;
+	}
+
+	/* send stream request. */
+	msg = "<stream:stream to='gmail.com' xmlns='jabber:client'"
+		" xmlns:stream='http://etherx.jabber.org/streams' version='1.0'>";
+
+	if (send(sk, msg, strlen(msg), 0) == -1) {
+
+		hybrid_debug_error("xmpp", "send initial jabber request failed");
+
+		return FALSE;
+	}
+
+	hybrid_event_add(sk, HYBRID_EVENT_READ, init_stream_cb, NULL);
+
+	return FALSE;
+}
+
 static gboolean
 xmpp_login(HybridAccount *account)
 {
-	printf("%s, %s\n", account->username, account->password);
+	hybrid_proxy_connect(jabber_server, 5222, init_connect_cb, NULL);
 
 	return FALSE;
 }
