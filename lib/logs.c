7712af3defff3b8cb9acc290f04fa715b2d0ce56
ljingb <ljb90@live.cn>
2014-01-26 19:41:01 +0800

    Fix:incorrect date information in log files.

--- lib/logs.c
+++ lib/logs.c
@@ -160,7 +160,7 @@ hybrid_logs_create(HybridAccount *account, const gchar *id)
 
     log->log_path = g_strdup_printf("%s/%d_%d_%d.xml",
                     final_path, local_time->tm_year + 1900,
-                    local_time->tm_mon, local_time->tm_mday);
+                    local_time->tm_mon + 1, local_time->tm_mday);
 
     g_free(final_path);
 
